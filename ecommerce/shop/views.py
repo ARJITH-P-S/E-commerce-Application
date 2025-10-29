87aad534b8943c36ff211a6b8f355166d424be19
ARJITH-P-S <arjithpsajithpkd@gmail.com>
2025-10-29 20:29:59 +0530

    Initial commit

---
+++ ecommerce/shop/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views import View
+from shop.models import Category
+
+class Categoryview(View):
+    def get(self,request):
+        c = Category.objects.all()
+        context = {'categories':c}
+        return render(request,'categories.html',context)
+
+class ProductView(View):
+    def get(self,request,i):
+        c = Category.objects.get(id=i)
+        context = {'category':c}
+        return render(request,'products.html',context)
